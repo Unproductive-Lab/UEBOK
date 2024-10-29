84a4c2137bcb3ec5a27c3b6bcf97d430ef22a1a5
yepitslap <yungvenuz@bk.ru>
2024-10-29 18:19:41 +0300

    init commit

---
+++ main.cmd
@@ -0,0 +1,25 @@
+@echo OFF
+chcp 65001
+setlocal enabledelayedexpansion
+
+net session >nul 2>&1
+if %errorLevel% equ 0 ( echo uspeh
+) else (
+echo               -. .
+echo        _____   ',' ,
+echo      ,'     ,'   ', ',
+echo    ,'     ,'      \  /
+echo   \       \       /  \
+echo     \ /^\   \    ,' ,'
+echo           \   \ ,' ,'
+echo     / ~-.___\.-'  ,'
+echo   /   .______.- ~ \
+echo /   /'          \   \
+echo \./               \/'
+
+echo ДАЁЖ АДМИН ПРАВА РАБОЧЕМУ НАРОДУ
+
+echo ---------------------------------
+echo включи скрипт с админ правами
+)
+pause
